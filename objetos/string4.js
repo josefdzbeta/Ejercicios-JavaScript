97cc09506002ab64755b4b0145ab9a9698b10c63
josefdzbeta <jafb19@outlook.com>
2021-12-15 01:52:42 +0100

    ejercicio objetos

--- objetos/string4.js
+++ objetos/string4.js
@@ -1,2 +1,16 @@
 'use strict'
 
+
+
+function encontrarCadena(){
+    let cadena = prompt('Introduce una cadena')
+    let clave = prompt('Palabra a buscar')
+
+    if(cadena.includes(clave)){
+        console.log('palabra encontrada' + 'en la posicion cadena.search(clave)')
+    }else{
+        console.log('no se lo que es una leyenda')
+    }
+
+}
+encontrarCadena()
